9e147f808b5b52e892358ef96c9f4abda4bf0507
woshahua <wohangye@gmail.com>
2023-06-22 13:23:15 +0900

    update

--- init.el
+++ init.el
@@ -581,7 +581,7 @@
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory (file-truename "/Users/han-ko/org-roam/"))
+  (org-roam-directory (file-truename "/Users/han-ko/Library/Mobile Documents/com~apple~CloudDocs/org-roam/"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
